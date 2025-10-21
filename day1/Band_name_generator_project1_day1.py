ef76821187aba1f1540f0fbe3f0811a242779f1a
Safa Tahreem <safatahreem23@gmail.com>
2025-10-21 05:18:40 -0400

    Add files via upload

---
+++ day1/Band_name_generator_project1_day1.py
@@ -0,0 +1,7 @@
+# DAY1_PROJECT1 : BAND NAME GENERATOR
+print("Welcome to the Band name Generator")
+
+city = input("What is the name of the city you grew in?\n")
+pet = input("What's your pet name?")
+
+print("Your Band Name could be"+" "+city+" "+pet)
---
+++ day1/day1_practice.py
@@ -0,0 +1,38 @@
+# print() funtion- to print output on the screen
+"""print("Hello!")
+print("Hello World")
+print("Hello World!This is Safa Tahreem")
+print("Iam learning Python Programming")
+print("I have learnt how to print statement using print function")"""
+
+# To print the statement into new line we use \n
+"""print("Hello \nWorld")
+print("Hello World!\nThis is Safa Tahreem")
+print("Safa\nTahreem")"""
+
+#input() funtion- to take input from the user and print on the screen
+"""name = input("What is your Name?")
+print(name)
+"""
+"""age = input("What is your age?")
+print(age)"""
+
+# For space between two strings we use double quotes
+"""print("Hello"+ " " +input("what is your name?"))
+name = "jack"
+print(name)
+
+name = "safa"
+print(name)"""
+
+# To find the length of the string - len() function
+"""name = "safa"
+print(len(name))
+
+name = "safa Tahreem" #len=14 empty space also gets counted here
+print(len(name))
+
+subject = input("Which subject you dislike the most?")
+length = len(subject)
+print(length)"""
+
